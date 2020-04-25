85a025d644ad50ae0c211df894ae9a78a43350c8
Kogan <koganp42@gmail.com>
2020-04-25 14:26:53 -0500

    Increased number of users requested in api

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
   getRandomUsers: function() {
-    return axios.get("https://randomuser.me/api/?page=1&results=10&seed=abc");
+    return axios.get("https://randomuser.me/api/?page=1&results=50&seed=abc");
   }
 };
